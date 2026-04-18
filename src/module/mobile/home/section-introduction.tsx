c1a855f431930af50168f80a86435537af0696a4
GWD <priasetia6282@gmail.com>
2026-04-19 03:24:16 +0700

    Update introduction text in section-introduction.tsx

--- src/module/mobile/home/section-introduction.tsx
+++ src/module/mobile/home/section-introduction.tsx
@@ -3,7 +3,7 @@ export default function SectionIntroductionMobile() {
     <div className="CONTAINER snap-y snap-mandatory">
       <div className="flex h-screen w-full snap-center flex-col items-center justify-center">
         <h1 className="MENU-CHANGE-Y-200 text-center font-display text-6xl leading-[1.3] text-secondary md:text-8xl xl:text-9xl 2xl:text-[10rem]">
-          Let Me Introduce My Self As Developer
+          Waste your time And the world Will waste you
         </h1>
       </div>
       <div className="flex h-screen w-full snap-center flex-col items-center justify-center">
