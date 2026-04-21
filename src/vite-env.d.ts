9c0790951d6437c4ff6970d0654edf721de15d68
GWD <priasetia6282@gmail.com>
2026-04-21 17:27:55 +0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,21 @@
 declare module 'scribby'
 declare module 'meanderer'
 declare module 'hover-effect'
+
+
+/// <reference types="vite/client" />
+
+declare module '*.mpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.m4a' {
+  const src: string;
+  export default src;
+}
