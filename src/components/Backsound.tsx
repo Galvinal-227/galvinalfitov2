4103ed3aa18fb595ce696009bf9552aae10cbc8b
GWD <priasetia6282@gmail.com>
2026-04-21 23:56:19 +0700

    Update Backsound.tsx

--- src/components/Backsound.tsx
+++ src/components/Backsound.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Volume2, VolumeX, ArrowRight } from 'lucide-react';
 import { useMusic } from 'context/MusicContext';
-import Music from '../assets/animation/music.json';
+import Music from 'assets/animation/music.json';
 import Lottie from 'lottie-react';
 
 interface BacksoundProps {
